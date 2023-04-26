ba164b4d43d89f3b51328c2468acf092be32a3d1
thinkofyashh <103776503+thinkofyashh@users.noreply.github.com>
2023-04-26 22:07:54 +0530

    Add files via upload

---
+++ MinimumInsertionToMake_a_ StringPalindrome/SpaceOptimisation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+int lcs(string s, string t)
+{
+	//Write your code here
+	int n=s.size();
+	int m=t.size();
+	
+    vector<int> prev(m+1,0);
+    vector<int>curr(m+1,0);
+	for(int j=0;j<=m;j++){
+		prev[j]=0;
+	}
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			 if (s[i-1] == t[j-1]) {
+              curr[j]=1 + prev[j - 1];
+             }
+        else curr[j]=0+max(prev[j],curr[j-1]);
+
+		}
+        prev=curr;
+	}
+	return prev[m];
+}
+int longestPalindromeSubsequence(string s)
+{
+    // Write your code here.
+    string t=s;
+    reverse(t.begin(),t.end());
+    return lcs(s,t);
+} 
+int minInsertion(string &str)
+{
+    // Write your code here.
+    return str.size()-longestPalindromeSubsequence(str);
+}
