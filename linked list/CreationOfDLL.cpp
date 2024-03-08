0daa2e0d2435a0aca7fd24446c46bfcdffcc0e85
thinkofyashh <103776503+thinkofyashh@users.noreply.github.com>
2024-03-08 14:51:53 +0530

    CreationOfDLL

---
+++ linked list/CreationOfDLL.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    
+    Node(int data,Node*next,Node*back){
+        this->data=data;
+        this->next=next;
+        this->back=back;
+    }
+    
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+        this->back=nullptr;
+    }
+    
+};
+
+int main(){
+
+
+  
+}
