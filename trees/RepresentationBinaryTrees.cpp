cfe1dd79a94bba5081c5e4f30edef27534322fdd
thinkofyashh <103776503+thinkofyashh@users.noreply.github.com>
2024-05-06 20:08:35 +0530

    RepresentationBinaryTrees

---
+++ trees/RepresentationBinaryTrees.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ struct Node{
+     int data ;
+     struct Node*left;
+     struct Node*right;
+     Node(int val){
+         data=val;
+         left=right=nullptr;
+     }
+ };
+ 
+ int  main(){
+     struct Node* root=new Node(1);
+     root->left=new Node(2);
+     root->right=new Node(3);
+     root->left->right=new Node(4);
+     
+     return 0;
+ }
