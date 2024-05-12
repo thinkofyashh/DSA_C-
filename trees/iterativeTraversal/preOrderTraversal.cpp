6c61886b72048bbd2085f69d99a98f58b297e7f2
thinkofyashh <103776503+thinkofyashh@users.noreply.github.com>
2024-05-12 15:15:31 +0530

    preOrderTraversal.

---
+++ trees/iterativeTraversal/preOrderTraversal.cpp
@@ -0,0 +1,15 @@
+ vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==nullptr) return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node=st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left); 
+        }
+        return ans;
+        
+    }
