901e1991846f9b5be848eed7abf1bb5f70901082
thinkofyashh <103776503+thinkofyashh@users.noreply.github.com>
2024-05-23 17:09:50 +0530

    TopViewOfBinaryTree.

---
+++ trees/TopViewOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+vector<int> getTopView(TreeNode<int> *root)
+{
+    // Write your code here.
+    vector<int>ans;
+    if(root==nullptr) return ans;
+    map<int,int>mp;
+    queue<pair<TreeNode<int>*,int>>q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it =q.front();
+        q.pop();
+        TreeNode<int>*node=it.first;
+        int line=it.second;
+        if(mp.find(line)==mp.end()){
+            mp[line]=node->data;
+        }
+        if(node->left){
+            q.push({node->left, line - 1});
+        }
+        if(node->right){
+            q.push({node->right,line+1
+        });
+        }
+    }
+    for(auto it :mp){
+        ans.push_back(it.second);
+    }
+    return ans;
+
+}
